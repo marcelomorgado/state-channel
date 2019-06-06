8b8a23e9c32620dc7e6eefa123803829afe3e0da
cmarcelo <cmarcelom@gmail.com>
2019-06-06 12:25:38 +0100

    WIP contract

--- contracts/StateChannels.sol
+++ contracts/StateChannels.sol
@@ -1,6 +1,43 @@
 pragma solidity 0.5.8;
 
-contract StateChannels {
+
+contract ECVerify {
+    event LogNum(uint8 num);
+    event LogNum256(uint256 num);
+    event LogBool(bool b);
+    function ecrecovery(bytes32 hash, bytes memory sig) public returns (address) {
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+
+        // FIXME: Should this throw, or return 0?
+        require(sig.length == 65, "");
+
+        // The signature format is a compact form of:
+        //   {bytes32 r}{bytes32 s}{uint8 v}
+        // Compact means, uint8 is not padded to 32 bytes.
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := mload(add(sig, 65))
+        }
+
+        // old geth sends a `v` value of [0,1], while the new, in line with the YP sends [27,28]
+        if (v < 27)
+          v += 27;
+
+        return ecrecover(hash, v, r, s);
+    }
+
+    function ecverify(bytes32 hash, bytes memory sig, address signer) public returns (bool b) {
+        b = ecrecovery(hash, sig) == signer;
+        emit LogBool(b);
+        return b;
+    }
+}
+
+
+contract StateChannels is ECVerify {
     // the data structure for the channel
     struct Channel {
         bytes32 channelId;
@@ -15,6 +52,7 @@ contract StateChannels {
     mapping(bytes32 => Channel) channels;
 
     event ChannelOpened(bytes32 channelId);
+    event CounterPartyJoined(bytes32 channelId);
 
     // TODO: Generate channelId
     function open(bytes32 channelId, address address1, uint value) public payable {
@@ -49,5 +87,50 @@ contract StateChannels {
         require(msg.value != 0, "incorrect funds");
 
         channels[channelId].balance1 = msg.value;
+
+        emit CounterPartyJoined(channelId);
+    }
+
+    function close(
+        bytes32 channelId,
+        uint sequenceNumber,
+        uint balance0,
+        uint balance1,
+        bytes memory signature0,
+        bytes memory signature1
+    ) public {
+
+        require(channels[channelId].channelId != 0, "no channel with that channelId exists");
+
+        // copies the channel from storage into memory
+        Channel memory channel = channels[channelId];
+
+        require(channel.address0 == msg.sender || channel.address1 == msg.sender, "you are not a participant in this channel");
+
+        // sha3
+        bytes32 stateHash = keccak256(abi.encodePacked(
+            channelId,
+            balance0,
+            balance1,
+            sequenceNumber
+        ));
+
+        require(ecverify(stateHash, signature0, channel.address0), "signature0 invalid");
+
+        require(ecverify(stateHash, signature1, channel.address1), "signature1 invalid");
+
+        require(sequenceNumber > channel.sequenceNumber, "sequence number too low");
+
+        require((balance0 + balance1) == (channel.balance0 + channel.balance1), "the law of conservation of total balances was not respected");
+
+        // delete channel storage first to prevent re-entry
+        delete channels[channelId];
+
+        address payable a0 = address(uint160(channel.address0));
+        address payable a1 = address(uint160(channel.address1));
+
+        a0.transfer(balance0);
+        a1.transfer(balance1);
     }
 }
+
--- test/StateChannels.test.js
+++ test/StateChannels.test.js
@@ -1,18 +1,49 @@
 /* eslint-disable security/detect-non-literal-fs-filename */
 const StateChannels = artifacts.require('StateChannels');
+const { utils, eth } = web3;
+const { BN, toWei, randomHex } = utils;
+const { getBalance } = eth;
+
+// Tech-debt: Extract to testHelpers
+const expectEvent = (tx, eventName, expectedArgs) => {
+  const { logs } = tx;
+  const log = logs.find(l => l.event === eventName);
+  expect(log, `Event '${eventName}' not found`).to.be.ok;
+  const { args } = log;
+  expect(args).to.include(expectedArgs);
+};
 
 contract('StateChannels', accounts => {
-  const [ana, bob] = accounts;
-  let sc;
+  const [alice, bob] = accounts;
+  let contract;
+  let channelId;
+  const aliceValue = toWei('10', 'ether');
+  const bobValue = toWei('5', 'ether');
 
-  beforeEach(async () => {
-    sc = await StateChannels.new();
+  before(async () => {
+    contract = await StateChannels.new();
+    channelId = randomHex(32);
   });
 
-  it('should open a channel', async () => {
-    const channelId = web3.utils.asciiToHex('1');
-    const value = web3.utils.toWei('10', 'ether');
+  it('alice should open a channel', async () => {
+    const tx = await contract.open(channelId, bob, aliceValue, { from: alice, value: aliceValue });
 
-    await sc.open(channelId, bob, value, { from: ana, value });
+    expectEvent(tx, 'ChannelOpened', { channelId });
+
+    const expectedBalance = aliceValue;
+    const balance = await getBalance(contract.address);
+    expect(balance).to.equal(expectedBalance);
   });
+
+  it('bob should join to the channel', async () => {
+    const tx = await contract.join(channelId, { from: bob, value: bobValue });
+
+    expectEvent(tx, 'CounterPartyJoined', { channelId });
+
+    const expectedBalance = new BN(aliceValue).add(new BN(bobValue)).toString();
+    const balance = await getBalance(contract.address);
+    expect(balance).to.equal(expectedBalance);
+  });
+
+  it.skip("should get error if channel id doesn't exists");
 });
