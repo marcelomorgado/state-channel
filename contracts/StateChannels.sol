c8bb323ffd75791cb01c3906eae15c909016ac2a
cmarcelo <cmarcelom@gmail.com>
2019-06-06 15:14:17 +0100

    WIP State Channel

--- contracts/StateChannels.sol
+++ contracts/StateChannels.sol
@@ -1,17 +1,18 @@
 pragma solidity 0.5.8;
 
+// /import 'openzeppelin-solidity/contracts/cryptography/ECDSA.sol';
 
 contract ECVerify {
     event LogNum(uint8 num);
     event LogNum256(uint256 num);
     event LogBool(bool b);
-    function ecrecovery(bytes32 hash, bytes memory sig) public returns (address) {
+    function ecrecovery(bytes32 hash, bytes memory sig) public pure returns (address) {
         bytes32 r;
         bytes32 s;
         uint8 v;
 
         // FIXME: Should this throw, or return 0?
-        require(sig.length == 65, "");
+        require(sig.length == 65, "Sign should have length of 65");
 
         // The signature format is a compact form of:
         //   {bytes32 r}{bytes32 s}{uint8 v}
@@ -23,19 +24,18 @@ contract ECVerify {
         }
 
         // old geth sends a `v` value of [0,1], while the new, in line with the YP sends [27,28]
-        if (v < 27)
-          v += 27;
+        if (v < 27) v += 27;
 
         return ecrecover(hash, v, r, s);
     }
 
     function ecverify(bytes32 hash, bytes memory sig, address signer) public returns (bool b) {
-        b = ecrecovery(hash, sig) == signer;
+        bytes32 ethHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
+        b = ecrecovery(ethHash, sig) == signer;
         emit LogBool(b);
         return b;
     }
 }
-
 
 contract StateChannels is ECVerify {
     // the data structure for the channel
@@ -53,6 +53,7 @@ contract StateChannels is ECVerify {
 
     event ChannelOpened(bytes32 channelId);
     event CounterPartyJoined(bytes32 channelId);
+    event ChannelClosed(bytes32 channelId);
 
     // TODO: Generate channelId
     function open(bytes32 channelId, address address1, uint value) public payable {
@@ -99,29 +100,34 @@ contract StateChannels is ECVerify {
         bytes memory signature0,
         bytes memory signature1
     ) public {
-
         require(channels[channelId].channelId != 0, "no channel with that channelId exists");
 
         // copies the channel from storage into memory
         Channel memory channel = channels[channelId];
 
-        require(channel.address0 == msg.sender || channel.address1 == msg.sender, "you are not a participant in this channel");
+        require(
+            channel.address0 == msg.sender || channel.address1 == msg.sender,
+            "you are not a participant in this channel"
+        );
 
         // sha3
-        bytes32 stateHash = keccak256(abi.encodePacked(
-            channelId,
-            balance0,
-            balance1,
-            sequenceNumber
-        ));
+        bytes32 stateHash = keccak256(
+            abi.encodePacked(channelId, balance0, balance1, sequenceNumber)
+        );
+
+       
 
         require(ecverify(stateHash, signature0, channel.address0), "signature0 invalid");
 
         require(ecverify(stateHash, signature1, channel.address1), "signature1 invalid");
 
+        // TODO: Remove-me?
         require(sequenceNumber > channel.sequenceNumber, "sequence number too low");
 
-        require((balance0 + balance1) == (channel.balance0 + channel.balance1), "the law of conservation of total balances was not respected");
+        require(
+            (balance0 + balance1) == (channel.balance0 + channel.balance1),
+            "the law of conservation of total balances was not respected"
+        );
 
         // delete channel storage first to prevent re-entry
         delete channels[channelId];
@@ -131,6 +137,7 @@ contract StateChannels is ECVerify {
 
         a0.transfer(balance0);
         a1.transfer(balance1);
+
+        emit ChannelClosed(channelId);
     }
 }
-
