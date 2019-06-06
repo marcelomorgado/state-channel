6a834a9632579f0cc828f4b6207bce9cd35dd6be
cmarcelo <cmarcelom@gmail.com>
2019-06-06 21:45:06 +0100

    Question 1

--- contracts/TokenChannels.sol
+++ contracts/TokenChannels.sol
@@ -6,14 +6,8 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 // TODO
 //
-// Rename vars
-// Use SafeMath
-// Use modifers
 // Challenge
-// Offline cases (periods)
 // ChannelState
-// Great comments
-// Review error messages
 // Safety review
 //
 contract TokenChannels {
@@ -32,13 +26,18 @@ contract TokenChannels {
         uint nonce;
     }
 
-    // channels by Id
     mapping(bytes32 => Channel) public channels;
 
+    //
+    // Events
+    //
     event ChannelOpened(bytes32 channelId);
     event CounterPartyJoined(bytes32 channelId);
     event ChannelClosed(bytes32 channelId);
 
+    //
+    // Modifiers
+    //
     modifier validChannel(bytes32 id) {
         require(channels[id].channelId != 0, "no channel with that channelId exists");
         _;
@@ -52,15 +51,25 @@ contract TokenChannels {
         _;
     }
 
+    //
+    // Public functions
+    //
+
+    /**
+   * Open a new channel. That contract should be approved by the sender to transfer on his behalf
+   *
+   * @param tokenAddress        Address of the token contract
+   * @param counterPartyAddress Account address of the other party
+   * @param amount              Number of tokens to deposit
+   */
     function open(address tokenAddress, address counterPartyAddress, uint256 amount) public {
         address partyAddress = msg.sender;
 
-        require(partyAddress != counterPartyAddress, "you cant create a channel with yourself");
-        require(amount != 0, "you can't create a payment channel with no money");
+        require(partyAddress != counterPartyAddress, "You can't create a channel with yourself");
+        require(amount != 0, "You can't create a payment channel without tokens");
 
-        // Note: block.timestamp isn't a strong source of entropy but it's safe enough for that use case
         bytes32 channelId = keccak256(
-            abi.encodePacked(partyAddress, counterPartyAddress, block.timestamp)
+            abi.encodePacked(tokenAddress, partyAddress, counterPartyAddress, block.number)
         );
 
         Channel memory channel = Channel(
@@ -70,17 +79,26 @@ contract TokenChannels {
             counterPartyAddress,
             amount, // partyBalance
             0, // counterPartyBalance
-            0 // sequence number
+            0 // nonce
         );
 
         ERC20 token = ERC20(tokenAddress);
-        require(token.transferFrom(partyAddress, address(this), amount), "incorrect funds");
+        require(
+            token.transferFrom(partyAddress, address(this), amount),
+            "Token transfer with error"
+        );
 
         channels[channelId] = channel;
 
         emit ChannelOpened(channelId);
     }
 
+    /**
+   * Join to an existent channel. That contract should be approved by the sender to transfer on his behalf
+   *
+   * @param channelId   Channel ID
+   * @param amount      Number of tokens to deposit
+   */
     function join(bytes32 channelId, uint256 amount) public validChannel(channelId) {
         address counterPartyAddress = msg.sender;
 
@@ -88,18 +106,34 @@ contract TokenChannels {
 
         require(
             channel.counterPartyAddress == counterPartyAddress,
-            "the channel creator did not specify you as the second participant"
+            "The channel creator did'nt specify you as the counter party"
         );
-        require(amount != 0, "incorrect funds");
 
-        ERC20 token = ERC20(channel.tokenAddress);
-        require(token.transferFrom(counterPartyAddress, address(this), amount), "incorrect funds");
+        // Tech-debt: Is this check vulnarable?
+        require(amount >= 0, "Incorrect amount");
 
-        channel.counterPartyBalance = amount;
+        if (amount > 0) {
+            ERC20 token = ERC20(channel.tokenAddress);
+            require(
+                token.transferFrom(counterPartyAddress, address(this), amount),
+                "Token transfer with error"
+            );
+            channel.counterPartyBalance = amount;
+        }
 
         emit CounterPartyJoined(channelId);
     }
 
+    /**
+   * Clone a channel
+   *
+   * @param channelId               Channel ID
+   * @param nonce                   Sequence number
+   * @param partyBalance            The final balance of the party
+   * @param counterPartyBalance     The final balance of the counter party
+   * @param partySignature          Last state of the channel signed by the party
+   * @param counterPartySignature   Last state of the channel signed by the counter party
+   */
     function close(
         bytes32 channelId,
         uint nonce,
@@ -144,13 +178,25 @@ contract TokenChannels {
         emit ChannelClosed(channelId);
     }
 
-    function ecverify(bytes32 hash, bytes memory sig, address signer)
+    //
+    // Internal functions
+    //
+
+    /**
+   * Check if a hash was signed by an address
+   * Note: That function was tested using signatures by web3.eth.sign()
+   *
+   * @param hash        Hash data
+   * @param signature   Signature to check
+   * @param signer      Signer to check
+   */
+    function ecverify(bytes32 hash, bytes memory signature, address signer)
         internal
         pure
         returns (bool b)
     {
         bytes32 ethHash = hash.toEthSignedMessageHash();
-        return ethHash.recover(sig) == signer;
+        return ethHash.recover(signature) == signer;
     }
 
 }
